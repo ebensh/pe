645ba2810f9ce8c96a4990916f7e36620d8f97aa
EvanB <ebensh@gmail.com>
2015-02-02 22:43:30 -0500

    pe72 initial commit

---
+++ pe72.py
@@ -0,0 +1,10 @@
+import cPickle
+
+# Primes will always add d-1
+def main():
+  
+  
+  for d in xrange(
+
+if __name__=="__main__":
+  main()
--- picklePrimes.py
+++ picklePrimes.py
@@ -6,4 +6,4 @@ def main(maxPrime, outputPath):
     cPickle.dump(list(sieve.primerange(2, maxPrime)), f, cPickle.HIGHEST_PROTOCOL)
 
 if __name__=="__main__":
-  main(100000000, 'primes_to_100000000.p')
+  main(1000000, 'primes_to_1000000.p')
