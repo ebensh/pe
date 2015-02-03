9786a4dc9a5818f3ffae2be7bd2aa96c2255eada
EvanB <ebensh@gmail.com>
2015-02-02 22:52:21 -0500

    72 wip

--- pe72.py
+++ pe72.py
@@ -1,10 +1,22 @@
 import cPickle
+import fractions
 
 # Primes will always add d-1
 def main():
+  primes = None
+  with open('primes_to_1000000.p', 'r') as f:
+    primes = set(cPickle.load(f))
   
-  
-  for d in xrange(
+  count = 0
+  for d in xrange(2, 1000000+1):
+    print d, count
+    if d in primes:
+      count += d - 1  # eg. 1/3, 2/3
+      continue
+    for n in xrange(1, d):
+      if fractions.gcd(n, d):
+        count += 1
+  print "Done :) Total:", count
 
 if __name__=="__main__":
   main()
