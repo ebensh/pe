5f270908ec6cb4a5ed6927d0fa2b40aee3a3a3c0
EvanB <ebensh@gmail.com>
2015-02-02 23:50:48 -0500

    pe92.py is slow but works

---
+++ pe92.py
@@ -0,0 +1,24 @@
+import math
+
+def mutate(x):
+  return sum([int(d) ** 2 for d in str(x)])
+
+def main():
+  kMax = 10000000
+  low = set([1])
+  high = set([89])
+  for x in xrange(2, kMax):
+    xs = [x]
+    while True:
+      if x == 1 or x in low:
+        low.update(xs)
+        break
+      elif x in high:
+        high.update(xs)
+        break
+      x = mutate(x)
+      xs.append(x)
+  print len(high)
+
+if __name__=="__main__":
+  main()
